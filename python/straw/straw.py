131cac92e8817c269f08f9bc037058e1a44910a2
rraadd88 <rohanadandage@gmail.com>
2020-04-06 10:25:56 -0400

    update

--- python/straw/straw.py
+++ python/straw/straw.py
@@ -84,6 +84,90 @@ class ChromDotSizes:
 
         return chrom, indx1, indx2
 
+def read_metadata(infile,verbose=True):
+    """
+    Reads the metadata of HiC file from header.
+
+    Args
+    infile: str, path to the HiC file 
+    verbose: bool, print the metadata if True else return the metadata
+    
+    Returns
+    metadata: dict, the metadata is returned if verbose is False. 
+                Keys of the metadata: 
+                HiC version, 
+                Master index, 
+                Genome ID (str), 
+                Attribute dictionary (dict), 
+                Chromosomes (dict), 
+                chromosomes2length (dict), 
+                Base pair-delimited resolutions (list), 
+                Fragment-delimited resolutions (list). 
+    """
+    metadata={}
+    import io
+    import struct
+    if (infile.startswith("http")):
+        # try URL first. 100K should be sufficient for header
+        headers={'range' : 'bytes=0-100000', 'x-amz-meta-requester' : 'straw'}
+        s = requests.Session()
+        r=s.get(infile, headers=headers)
+        if (r.status_code >=400):
+            print("Error accessing " + infile) 
+            print("HTTP status code " + str(r.status_code))
+            sys.exit(1)
+        req=io.BytesIO(r.content)        
+        myrange=r.headers['content-range'].split('/')
+        totalbytes=myrange[1]
+    else:
+        req=open(infile, 'rb')
+    magic_string = struct.unpack('<3s', req.read(3))[0]
+    req.read(1)
+    if (magic_string != b"HIC"):
+        sys.exit('This does not appear to be a HiC file magic string is incorrect')
+    version = struct.unpack('<i',req.read(4))[0]
+    metadata['HiC version']=version 
+    masterindex = struct.unpack('<q',req.read(8))[0]
+    metadata['Master index']=masterindex
+    genome = ""
+    c=req.read(1).decode("utf-8") 
+    while (c != '\0'):
+        genome += c
+        c=req.read(1).decode("utf-8") 
+    metadata['Genome ID']=genome        
+    ## read and throw away attribute dictionary (stats+graphs)
+    nattributes = struct.unpack('<i',req.read(4))[0]
+    d={}
+    for x in range(0, nattributes):
+        key = __readcstr(req)
+        value = __readcstr(req)
+        d[key]=value
+    metadata['Attribute dictionary']=d
+    nChrs = struct.unpack('<i',req.read(4))[0]
+    d={}
+    for x in range(0, nChrs):
+        key = __readcstr(req)
+        value = struct.unpack('<i',req.read(4))[0]
+        d[key]=value
+    metadata["Chromosomes"]=d
+    metadata["chromosomes2length"]=metadata["Chromosomes"]
+    nBpRes = struct.unpack('<i',req.read(4))[0]
+    l=[]
+    for x in range(0, nBpRes):
+        res = struct.unpack('<i',req.read(4))[0]
+        l.append(res)
+    metadata["Base pair-delimited resolutions"]=l        
+    nFrag = struct.unpack('<i',req.read(4))[0]
+    l=[]
+    for x in range(0, nFrag):
+        res = struct.unpack('<i',req.read(4))[0]
+        l.append(res)
+    metadata["Fragment-delimited resolutions"]=l 
+    if verbose:
+        for k in metadata:
+            print(k,':',metadata[k])
+    else:
+        return metadata    
 
 def readHeader(infile, is_synapse):
     """ Reads the header
