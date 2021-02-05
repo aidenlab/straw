90367afab2f4142860f53567a6cb5a1d26a007a9
nchernia <nchernia@gmail.com>
2021-02-05 11:06:18 -0500

    Updated read header to handle v9. Fix #58

--- python/straw/straw.py
+++ python/straw/straw.py
@@ -134,6 +134,11 @@ def read_metadata(infile,verbose=False):
         genome += c
         c=req.read(1).decode("utf-8") 
     metadata['Genome ID']=genome        
+    if (version > 8):
+        nvi = struct.unpack('<q',req.read(8))[0]
+        nvisize = struct.unpack('<q',req.read(8))[0]
+        metadata['NVI'] = nvi
+        metadata['NVI size'] = nvisize
     ## read and throw away attribute dictionary (stats+graphs)
     nattributes = struct.unpack('<i',req.read(4))[0]
     d={}
@@ -146,7 +151,10 @@ def read_metadata(infile,verbose=False):
     d={}
     for x in range(0, nChrs):
         key = __readcstr(req)
-        value = struct.unpack('<i',req.read(4))[0]
+        if (version > 8):
+            value = struct.unpack('q',req.read(8))[0]
+        else:
+            value = struct.unpack('<i',req.read(4))[0]
         d[key]=value
     metadata["Chromosomes"]=d
     nBpRes = struct.unpack('<i',req.read(4))[0]
