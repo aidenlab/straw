41af5a09eb01e8c1791dd4b0b5007da0bdc4e41f
Craig Wenger <craig.wenger@gmail.com>
2021-09-12 20:22:48 -0700

    Update versions from 0.0.8 to 0.0.9.

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="hic-straw",
-    version="0.0.8",
+    version="0.0.9",
     author="Neva C. Durand",
     email="theaidenlab@gmail.com",
     description="Extract data quickly from Juicebox hic files via straw",
