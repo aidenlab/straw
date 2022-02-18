2d5f46fe140845dfb67af32efcc456d2de42546a
Muhammad S Shamim <sa501428@gmail.com>
2022-02-18 11:56:56 -0600

    additional check to remove nans

--- pybind11_python/src/straw.cpp
+++ pybind11_python/src/straw.cpp
@@ -1075,11 +1075,13 @@ public:
                         }
                     }
 
-                    contactRecord record = contactRecord();
-                    record.binX = static_cast<int32_t>(x);
-                    record.binY = static_cast<int32_t>(y);
-                    record.counts = c;
-                    records.push_back(record);
+                    if (!isnan(c) && !isinf(c)){
+                        contactRecord record = contactRecord();
+                        record.binX = static_cast<int32_t>(x);
+                        record.binY = static_cast<int32_t>(y);
+                        record.counts = c;
+                        records.push_back(record);
+                    }
                 }
             }
         }
